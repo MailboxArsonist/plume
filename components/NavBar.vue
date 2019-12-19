9ec12a3d7c63e894a0a225e83ce5c727e9a18b46
Matthew Dodd <matthew.dodd93@gmail.com>
2019-12-19 10:27:59 +0100

    add nav scroll

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,10 +1,22 @@
 <template>
-  <nav class="nav">
-    <img src="../assets/img/svg/plume.svg" alt="plume-icon">
+  <nav
+    class="nav"
+    :class="{
+      'nav-minimized': minimizeNav
+    }"
+    @scroll="handleScroll"
+  >
+    <img src="../assets/img/svg/plume.svg" alt="plume-icon" />
     <ul class="nav-links">
-      <li class="nav-link"><a href="about.html">About</a></li>
-      <li class="nav-link"><a href="prestations.html">Prestations</a></li>
-      <li class="nav-link"><a href="contact.html">Contact</a></li>
+      <li class="nav-link">
+        <nuxt-link to="">About</nuxt-link>
+      </li>
+      <li class="nav-link">
+        <nuxt-link to="">Prestations</nuxt-link>
+      </li>
+      <li class="nav-link">
+        <nuxt-link to="">Contact</nuxt-link>
+      </li>
     </ul>
   </nav>
 </template>
@@ -12,54 +24,79 @@
 <script>
 export default {
   name: "NavBar",
-}
+  data() {
+    return {
+      minimizeNav: false
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+    if (window.scrollY > 50) {
+      this.minimizeNav = true;
+    } else if (document.body.scrollTop < 50) {
+      this.minimizeNav = false;
+    }
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 50) {
+        this.minimizeNav = true;
+      } else if (document.body.scrollTop < 50) {
+        this.minimizeNav = false;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-  .nav {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 10;
-    height: 10em;
-    color: black;
-    border-bottom: 1px solid transparent;
-    background: #fff;
-    transition: height 700ms;
-
-    
-    box-shadow: 0 10px 20px -4px rgba(0,0,0,.03);
-    border-color: #fff;
-    transition: height 700ms;
-}
-.nav::after {
-    clear: both;
-    content: ' ';
-}
-
 .nav {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  height: 9em;
+  color: black;
+  border-bottom: 1px solid transparent;
+  background: #fff;
+  transition: height 700ms;
+
+  img {
+    height: auto;
+    width: 20em;
+    margin-left: 2em;
+  }
 }
 
-.nav img {
-    height: auto;
-    min-width: 20em;
-    margin-left: 2em;
+.nav::after {
+  clear: both;
+  content: " ";
+}
+
+.nav-minimized {
+  height: 75px;
+  box-shadow: 0 10px 20px -4px rgba(0, 0, 0, 0.03);
+  border-color: #fff;
+  transition: height 700ms;
 }
 
 .nav-links {
-    width: 100%;
-    margin: 0;
-    padding: 1em;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    color: inherit;
+  width: 100%;
+  margin: 0;
+  padding: 1em;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  color: inherit;
 }
 
 .nav-link {
@@ -67,16 +104,16 @@ export default {
   display: inline;
   margin-right: 5em;
   a {
-    font-family: 'News Cycle', sans-serif;
-    font-size: 1.4em;
+    font-family: "News Cycle", sans-serif;
+    font-size: 1.2em;
     font-weight: 600;
     border-bottom: 1px solid transparent;
     position: relative;
     padding-left: 2em;
     &:before {
-      content: ' ';
+      content: " ";
       display: inline-block;
-      background: url('~assets/img/svg/nav-feather.svg');
+      background: url("~assets/img/svg/nav-feather.svg");
       height: 100%;
       background-position: center;
       background-repeat: no-repeat;
