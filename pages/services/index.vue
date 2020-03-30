738f662f61cc86fe7d4a57f95b2a8d9b8d15e760
Matthew Dodd <matthew.dodd93@gmail.com>
2020-03-30 16:19:51 +0200

    add section to services page

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,11 +1,14 @@
 <template>
   <main class="main">
-    <Service
-      v-for="service in services"
-      :key="service.title"
-      :service="service"
-      class="service"
-    />
+    <h1>These are the serviccees</h1>
+    <section class="services-section">
+      <Service
+        v-for="service in services"
+        :key="service.title"
+        :service="service"
+        class="service"
+      />
+    </section>
   </main>
 </template>
 
@@ -22,13 +25,13 @@ export default {
           title: "Brand messaging",
           message: "Timeline: 1 - 2 weeks",
           primaryColor: "#287392",
-          hoverColor: "black",
+          secondaryColor: "#1c5f7b",
           content: {
             title: "The brand collection",
             sections: [
               "You care deeply about your brand. But it doesn’t truly show your voice, your heart, and your vision. You need the support of a copywriting strategist to distill your voice into a brand message that genuinely connects, perfect for the business owner that wants to become fluent in their brand messaging strategy."
             ],
-            image: "",
+            image: "typing.jpg",
             button: {
               text: "Hit me up",
               type: "link",
@@ -45,5 +48,14 @@ export default {
 <style lang="scss" scoped>
 .service {
   margin: 1rem;
+}
+.services-section {
+  width: 90%;
+  max-width: 1100px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 }
 </style>
